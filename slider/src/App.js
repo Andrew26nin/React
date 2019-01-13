21e5492d08a8d80cf646ea23ba13ba897dcae2db
Andrew26nin <tran26@mail.ru>
2019-01-14 01:46:53 +0300

    Change function in App

--- slider/src/App.js
+++ slider/src/App.js
@@ -5,24 +5,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div className="wrapper">
+      <SayFullName name='Андрей' surname='Тарасенко' link='#'/>
+    </div>
     );
   }
+}
+
+function SayFullName(props) {
+  return(
+      <div>
+          <h1>Мое имя {props.name}, моя фамилия {props.surname}</h1>
+          <a href={props.link}>Ссылка на мой профиль</a>
+      </div>
+  )
 }
 
 export default App;
