fb0370bd7c9fa54d30a1b36d6822ccbf9f2eb037
Andrew26nin <tran26@mail.ru>
2019-01-14 21:56:52 +0300

    Update Main.js
    Доделано описание в маине

--- slider/src/components/Main.js
+++ slider/src/components/Main.js
@@ -17,6 +17,24 @@ span {
     line-height:30px;
 }
 `
+const LightText = styled.div`
+color: #ffffff;
+margin-top:54px;
+font-size: 18px;
+font-weight: 300;
+line-height: 28px;
+`
+const CallBtn= styled.button`
+width: 245px;
+height: 64px;
+background-color: #ffa14b;
+color: #ffffff;
+margin-top:32px;
+border-radius:30px;
+font-size: 18px;
+font-weight: bold;
+
+`
 
 class Main extends React.Component {
     render() {
@@ -25,9 +43,14 @@ class Main extends React.Component {
                 {/* Первый столбец с информаией */}
                 <Col lg="5">
                     <Repair>
-                    Качественный ремонт
+                        Качественный ремонт
 <span>iphone за 35 минут и гарантия 1 год</span>
                     </Repair>
+                    <LightText>Оставьте заявку на бесплатную диагностику без очереди,
+                                и получите защитное стекло, стоимостью 1000 рублей,
+                                с установкой в подарок!
+                    </LightText>
+<CallBtn>Отправить заявку!</CallBtn>
                 </Col>
                 <Col lg="6" lgOffset="1">
 
